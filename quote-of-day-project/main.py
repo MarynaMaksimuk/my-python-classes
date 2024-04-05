fcbc71882e8d6f54e11e4d1222b13f8a8cb1d890
Maryna Maksimuk <maksimarina@gmail.com>
2024-04-05 17:33:00 -0500

    Motivational quote of the day project - smtplib, datetime, pandas, random

---
+++ quote-of-day-project/main.py
@@ -0,0 +1,29 @@
+import smtplib
+import datetime as dt
+import random
+import pandas
+
+
+data = pandas.read_csv("email_data.csv")
+my_data = data.to_dict(orient="records")
+email = my_data[0]["my_email"]
+password = my_data[0]["password"]
+
+
+now = dt.datetime.now()
+day_of_week = now.weekday()
+if day_of_week == 4:
+    with open("quotes.txt") as data_file:
+        data = data_file.readlines()
+        today_quote = random.choice(data)
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=email, password=password)
+        connection.sendmail(from_addr=email, to_addrs=email, msg=f"Subject:Quote of the day"
+                                                                                    f"\n\n{today_quote}")
+
+
+
+
+
+
